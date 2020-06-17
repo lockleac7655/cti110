6fa10cbfe0f60cffc23acabc8f4192bc4233e9ed
lockleac7655 <67063011+lockleac7655@users.noreply.github.com>
2020-06-17 11:06:10 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_LocklearCalah.py
@@ -0,0 +1,16 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Get the projected total sales.
+>>> #06/15/2020
+>>> #CTI-110 P2T1 -Sales Prediction
+>>> #Calah Locklear
+>>> total_sales = float(input('Enter the projected sales: '))
+Enter the projected sales: 1000
+>>> Calculate the profit as 23 percent of total sales.
+SyntaxError: invalid syntax
+>>> #Calculate the profit as 23 percent of total sales.
+>>> profit = total_sales * 0.23
+>>> #Display the profit
+>>> print('The profit is $', format(profit, ',.2f'))
+The profit is $ 230.00
+>>> 
